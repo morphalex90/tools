1d94f81b37f1cf26cce622f3048eb37dd5bad4fa
morphalex90 <piero.nanni@gmail.com>
2020-06-17 17:41:38 +0100

    added footer with stars, added head

--- src/index.js
+++ src/index.js
@@ -20,17 +20,16 @@ class App extends React.Component {
   async getAverageStar() {
     try {
       let response = await fetch('https://phplaravel-382225-1341568.cloudwaysapps.com/api/v1/tools/average_star');
-      console.log(response);
-      // let responseJson = await response.json();
-      // console.log('test2: '+responseJson);
-      this.setState({ average_star: response.body.toString() });
+      // let response = await fetch('http://api.local/api/v1/tools/average_star');
+      let responseJson = await response.json();
+      this.setState({ average_star: responseJson });
      } catch(error) {
       console.error(error);
     }
   }
 
   render() {
-    document.title = 'Tools - Morpheus90' ; 
+    // document.title = 'Tools - Morpheus90' ; 
     // document.getElementsByTagName("META")[2].content="Your description about the page or site here to set dynamically";
 
     return (
@@ -48,42 +47,24 @@ class App extends React.Component {
             <div className="container-fluid">
                 <div className="row">
                     <h1 className="col-sm-12">Tools</h1>
-                    {/* {this.state.average_star.length === 0 && <div className="col-sm-12">Loading...</div>} */}
-                    <div>{this.state.average_star}</div>
 
-                    {/* {this.state.video.map(item => (
-                        <div key={item.video.title} className="col-xl-2 col-lg-3 col-md-4 col-sm-6 video">
-                            <div><a href={'/video/'+item.video.video_id} title={item.video.title}>{item.video.title}</a></div>
-                            <div className="video__image"><img src={this.convertImage(item.video.default_thumb, item.video.thumbs)} alt={item.video.title} /></div>
-                        </div>
-                    ))} */}
                 </div>
             </div>
         </section>
 
-        {/* <section>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-sm-12">
-                <form name="contact" method="POST">
-                  <input type="hidden" name="form-name" value="contact" />
-                  <p>
-                    <label>Your Name: <input type="text" name="name"/></label>
-                  </p>
-                  <p>
-                    <label>Your Email: <input type="email" name="email"/></label>
-                  </p>
-                  <p>
-                    <label>Message: <textarea name="message"></textarea></label>
-                  </p>
-                  <p>
-                    <button type="submit">Send</button>
-                  </p>
-              </form>
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-8">Idea &amp; code by <a href="https://www.pieronanni.com" target="_blank" title="Visit me!">Piero Nanni</a>, completely rebuilt during Hack Days at <a href="https://purrgroup.com" target="_blank" title="Purr Digital">Purr Digital</a></div>
+                    <div className="col-sm-4 text-right">
+                        <div itemProp="aggregateRating" itemScope itemType="http://schema.org/AggregateRating">
+                            <meta itemProp="bestRating" content="5" />
+                            {this.state.average_star.length === 0 && <span className="loading">*</span>}<span itemProp="ratingValue">{this.state.average_star}</span><span>/5 stars based on</span> <span itemProp="reviewCount">5</span> customer reviews                             
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </section> */}
+        </footer>
 
       </div>
     );
