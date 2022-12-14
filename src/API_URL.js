c352d4c4edd5bc4292099439caac22e5ceecc62c
morphalex90 <piero.nanni@gmail.com>
2022-12-14 22:49:34 +0000

    fix issue with env not readable

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,6 +1,5 @@
-export const API_URL = process.env.VITE_API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
 export default API_URL;
 
-
-import.meta.env.VITE_API_URL
+// import.meta.env.VITE_API_URL
