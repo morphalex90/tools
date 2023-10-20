5bd2bf9d3a330e597a70dd17969df5181ce71e0b
morphalex90 <piero.nanni@gmail.com>
2023-10-20 09:27:53 +0100

    update configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-import path from 'path'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const viteEnv = {}
-Object.keys(process.env).forEach((key) => {
-	if (key.startsWith(`VITE_`)) {
-		viteEnv[`import.meta.env.${key}`] = process.env[key]
-	}
-})
 
-export default {
-	alias: {
-		'@': require('path').resolve(__dirname, 'src')
-	},
-	define: viteEnv,
+// https://vitejs.dev/config/
+export default defineConfig({
 	plugins: [react()]
-}
+})
